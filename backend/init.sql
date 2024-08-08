0377f40d9647c53d4a2ff6a54f2ca74d1a23d245
Martin Caraballo <martin@martin.martin>
2024-08-08 10:44:19 -0300

    first commit

---
+++ backend/init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS app_reservas;
+
+USE app_reservas;
+
+CREATE TABLE IF NOT EXISTS Rooms(
+  room_id INT NOT NULL,
+  PRIMARY KEY (room_id)
+);
+
+CREATE TABLE IF NOT EXISTS Users(
+  email VARCHAR(64) NOT NULL,
+  name VARCHAR(32) NOT NULL,
+  last_name VARCHAR(32) NOT NULL,
+  authorized BOOLEAN NOT NULL,
+  PRIMARY KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS Admins(
+  email VARCHAR(64) NOT NULL,
+  PRIMARY KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS Login(
+  email VARCHAR(64) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  PRIMARY KEY (email),
+  FOREIGN KEY (email) REFERENCES Users(email)
+);
