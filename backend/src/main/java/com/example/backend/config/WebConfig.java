cb533f08c587c0fba4eaf71a6f47fb452cbe3f0a
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-10-02 17:48:21 -0300

    cors config update

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.example.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.zip.CheckedOutputStream;
+
 @Configuration
 @EnableWebMvc
 @EnableMethodSecurity
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origin}")
+    private String CORS_ORIGIN;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:80",
                         "http://localhost:80",
                         "http://127.0.0.1",
-                        "http://localhost"
+                        "http://localhost",
+                        CORS_ORIGIN
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
