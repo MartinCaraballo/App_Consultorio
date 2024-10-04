10430d3db312955856807284334a22921da2fc3d
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-10-04 15:34:14 -0300

    Changed @Data from FixedReserveKey and UserReserveKey to @Getter and @Setter

--- backend/src/main/java/com/example/backend/compositekeys/FixedReserveKey.java
+++ backend/src/main/java/com/example/backend/compositekeys/FixedReserveKey.java
@@ -3,11 +3,14 @@ package com.example.backend.compositekeys;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalTime;
 
-@Data
+@Getter
+@Setter
 @Embeddable
 public class FixedReserveKey implements Serializable {
     private Integer dayIndex;
--- backend/src/main/java/com/example/backend/compositekeys/UserReserveKey.java
+++ backend/src/main/java/com/example/backend/compositekeys/UserReserveKey.java
@@ -2,12 +2,15 @@ package com.example.backend.compositekeys;
 
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-@Data
+@Getter
+@Setter
 @Embeddable
 public class UserReserveKey implements Serializable {
 
