b67050940cc27ab29bc354b6d39d1a1dd3a2233d
Sergio Caballero <sergiocaballero1304@gmail.com>
2024-11-13 22:05:14 -0300

    loading animation complete

--- frontend/src/app/components/Modals/price-modification-modal.tsx
+++ frontend/src/app/components/Modals/price-modification-modal.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import axiosInstance from "@/utils/axios_instance";
 
 interface PriceModificationModalProps {
@@ -8,18 +8,25 @@ interface PriceModificationModalProps {
     onClose: () => void;
 }
 
-const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen, onClose }) => {
+const PriceModificationModal: React.FC<PriceModificationModalProps> = ({
+    isOpen,
+    onClose,
+}) => {
     const [prices, setPrices] = React.useState<Price[]>([]);
 
-    const [priceToSave, setPriceToSave] = React.useState<Price | undefined>(undefined);
+    const [priceToSave, setPriceToSave] = React.useState<Price | undefined>(
+        undefined
+    );
 
     const [addingPrice, setAddingPrice] = React.useState(false);
 
     const [showMessage, setShowMessage] = React.useState(false);
-    const [statusMessage, setStatusMessage] = React.useState('');
+    const [statusMessage, setStatusMessage] = React.useState("");
 
     const [hoursToPost, setHoursToPost] = React.useState<number>(0);
-    const [pricePerHoursToPost, setPricePerHourToPost] = React.useState<number>(0);
+    const [pricePerHoursToPost, setPricePerHourToPost] =
+        React.useState<number>(0);
+    const [loading, setLoading] = useState(true);
 
     // Fetch Prices only when the modal is active.
     React.useEffect(() => {
@@ -29,31 +36,45 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
     }, [isOpen]);
 
     async function fetchPrices() {
-        axiosInstance.get(`/admin/prices`)
-            .then(res => setPrices(res.data))
-            .catch(e => console.error(e));
+        setLoading(true);
+
+        axiosInstance
+            .get(`/admin/prices`)
+            .then((res) => setPrices(res.data))
+            .catch((e) => console.error(e))
+            .finally(() => setLoading(false));
     }
 
     async function addPrice() {
-        axiosInstance.post('/admin/prices', { hours: hoursToPost, pricePerHour: pricePerHoursToPost })
+        setLoading(true);
+        axiosInstance
+            .post("/admin/prices", {
+                hours: hoursToPost,
+                pricePerHour: pricePerHoursToPost,
+            })
             .then(() => {
                 setShowMessage(true);
-                setStatusMessage("Precio añadido con éxito.")
+                setStatusMessage("Precio añadido con éxito.");
             })
             .catch(() => {
                 setShowMessage(true);
-                setStatusMessage("Error al intentar agregar el precio.")
+                setStatusMessage("Error al intentar agregar el precio.");
             })
             .finally(() => {
                 fetchPrices();
                 setTimeout(() => setShowMessage(false), 2000);
+                setLoading(false);
             });
     }
 
     async function updatePrice(newPrice: Price) {
-        axiosInstance.put(
-            '/admin/prices',
-            { id: newPrice.id, hours: newPrice.hours, pricePerHour: newPrice.pricePerHour })
+        setLoading(true);
+        axiosInstance
+            .put("/admin/prices", {
+                id: newPrice.id,
+                hours: newPrice.hours,
+                pricePerHour: newPrice.pricePerHour,
+            })
             .then(() => {
                 setShowMessage(true);
                 setStatusMessage("Precio actualizado con éxito.");
@@ -61,28 +82,32 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
             })
             .catch(() => {
                 setShowMessage(true);
-                setStatusMessage("Error al intentar actualizar el precio.")
+                setStatusMessage("Error al intentar actualizar el precio.");
             })
             .finally(() => {
                 setTimeout(() => setShowMessage(false), 2000);
-                fetchPrices()
+                fetchPrices();
+                setLoading(false);
             });
     }
 
     async function deletePrice(id: number) {
-        axiosInstance.delete(`/admin/prices/${id}`)
+        setLoading(true);
+        axiosInstance
+            .delete(`/admin/prices/${id}`)
             .then(() => {
                 setShowMessage(true);
-                setStatusMessage("Precio eliminado con éxito.")
+                setStatusMessage("Precio eliminado con éxito.");
             })
             .catch(() => {
                 setShowMessage(true);
-                setStatusMessage("Error al intentar eliminar el precio.")
+                setStatusMessage("Error al intentar eliminar el precio.");
             })
             .finally(() => {
                 fetchPrices();
                 setTimeout(() => setShowMessage(false), 2000);
-            })
+                setLoading(false);
+            });
     }
 
     if (!isOpen) return null;
@@ -91,28 +116,81 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white rounded-lg shadow-md w-full max-w-2xl p-6 flex flex-col">
                 <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-2xl font-bold">Modificar Precios</h1>
-                    <button onClick={onClose} className="text-gray-500 text-2xl hover:text-gray-700">
+                    <div className="flex items-center">
+                        <h1 className="text-2xl font-bold">
+                            Modificar Precios
+                        </h1>
+                        <div className="pl-2">
+                            {loading && (
+                                <svg
+                                    className="animate-spin h-5 w-5 mr-3"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    ></circle>
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                    ></path>
+                                </svg>
+                            )}
+                        </div>
+                    </div>
+
+                    <button
+                        onClick={onClose}
+                        className="text-gray-500 text-2xl hover:text-gray-700"
+                    >
                         &times;
                     </button>
                 </div>
 
                 <ul className="space-y-2 pb-6">
                     {prices.map((price) => (
-                        <li key={price.id} className="p-2 border border-gray-300 rounded-xl flex flex-col lg:flex-row justify-between items-center">
+                        <li
+                            key={price.id}
+                            className="p-2 border border-gray-300 rounded-xl flex flex-col lg:flex-row justify-between items-center"
+                        >
                             <span>{`Horas: ${price.hours}, Precio por Hora: $${price.pricePerHour}`}</span>
                             <div className="flex items-center">
                                 <input
                                     type="number"
                                     name="hours"
-                                    value={priceToSave && priceToSave.id === price.id ? priceToSave.hours : price.hours}
+                                    value={
+                                        priceToSave &&
+                                        priceToSave.id === price.id
+                                            ? priceToSave.hours
+                                            : price.hours
+                                    }
                                     onChange={(e) => {
                                         let newPrice;
                                         let newValue = parseInt(e.target.value);
-                                        if (priceToSave && priceToSave.id === price.id) {
-                                            newPrice = {...priceToSave, hours: newValue > 144 ? 144 : newValue};
+                                        if (
+                                            priceToSave &&
+                                            priceToSave.id === price.id
+                                        ) {
+                                            newPrice = {
+                                                ...priceToSave,
+                                                hours:
+                                                    newValue > 144
+                                                        ? 144
+                                                        : newValue,
+                                            };
                                         } else {
-                                            newPrice = {...price, hours: newValue > 144 ? 144 : newValue};
+                                            newPrice = {
+                                                ...price,
+                                                hours:
+                                                    newValue > 144
+                                                        ? 144
+                                                        : newValue,
+                                            };
                                         }
                                         setPriceToSave(newPrice);
                                     }}
@@ -122,13 +200,31 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                 <input
                                     type="number"
                                     name="price_per_hour"
-                                    value={priceToSave && priceToSave.id === price.id ? priceToSave.pricePerHour : price.pricePerHour}
+                                    value={
+                                        priceToSave &&
+                                        priceToSave.id === price.id
+                                            ? priceToSave.pricePerHour
+                                            : price.pricePerHour
+                                    }
                                     onChange={(e) => {
                                         let newPrice;
-                                        if (priceToSave && priceToSave.id === price.id) {
-                                            newPrice = {...priceToSave, pricePerHour: parseInt(e.target.value)};
+                                        if (
+                                            priceToSave &&
+                                            priceToSave.id === price.id
+                                        ) {
+                                            newPrice = {
+                                                ...priceToSave,
+                                                pricePerHour: parseInt(
+                                                    e.target.value
+                                                ),
+                                            };
                                         } else {
-                                            newPrice = {...price, pricePerHour: parseInt(e.target.value)};
+                                            newPrice = {
+                                                ...price,
+                                                pricePerHour: parseInt(
+                                                    e.target.value
+                                                ),
+                                            };
                                         }
                                         setPriceToSave(newPrice);
                                     }}
@@ -137,10 +233,23 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                 />
                                 <div className="pr-2">
                                     <button
-                                        disabled={!(priceToSave && priceToSave.id === price.id)}
-                                        className={`p-1 ${priceToSave && priceToSave.id === price.id ? 'bg-green-500' : 'bg-gray-300'} text-white rounded`}
+                                        disabled={
+                                            !(
+                                                priceToSave &&
+                                                priceToSave.id === price.id
+                                            )
+                                        }
+                                        className={`p-1 ${
+                                            priceToSave &&
+                                            priceToSave.id === price.id
+                                                ? "bg-green-500"
+                                                : "bg-gray-300"
+                                        } text-white rounded`}
                                         onClick={() => {
-                                            if (priceToSave && priceToSave.id === price.id) {
+                                            if (
+                                                priceToSave &&
+                                                priceToSave.id === price.id
+                                            ) {
                                                 updatePrice(priceToSave);
                                             }
                                         }}
@@ -150,8 +259,9 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                 </div>
                                 <button
                                     onClick={() => deletePrice(price.id)}
-                                    className="p-1 bg-red-600 text-white rounded">
-                                    <FontAwesomeIcon icon={faTrashCan}/>
+                                    className="p-1 bg-red-600 text-white rounded"
+                                >
+                                    <FontAwesomeIcon icon={faTrashCan} />
                                 </button>
                             </div>
                         </li>
@@ -160,7 +270,9 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                 {addingPrice && (
                     <>
                         <div className="p-2 border-2 rounded-xl text-lg">
-                            <span className="text-2xl font-bold">Agregar Precio:</span>
+                            <span className="text-2xl font-bold">
+                                Agregar Precio:
+                            </span>
                             <div className="py-2 flex flex-row justify-evenly">
                                 <div>
                                     <span className="pr-2">Horas:</span>
@@ -170,7 +282,9 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                         value={hoursToPost}
                                         onChange={(e) => {
                                             try {
-                                                const newValue = parseInt(e.target.value);
+                                                const newValue = parseInt(
+                                                    e.target.value
+                                                );
                                                 setHoursToPost(newValue);
                                             } catch (e) {}
                                         }}
@@ -179,14 +293,18 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                     />
                                 </div>
                                 <div>
-                                    <span className="pr-1">Precio por Hora: $</span>
+                                    <span className="pr-1">
+                                        Precio por Hora: $
+                                    </span>
                                     <input
                                         type="number"
                                         name="adding-price-per-hour"
                                         value={pricePerHoursToPost}
                                         onChange={(e) => {
                                             try {
-                                                const newValue = parseInt(e.target.value);
+                                                const newValue = parseInt(
+                                                    e.target.value
+                                                );
                                                 setPricePerHourToPost(newValue);
                                             } catch (e) {}
                                         }}
@@ -201,7 +319,8 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                                         addPrice();
                                         setAddingPrice(false);
                                     }}
-                                    className="border-4 p-2 rounded-xl border-green-900 bg-green-500">
+                                    className="border-4 p-2 rounded-xl border-green-900 bg-green-500"
+                                >
                                     Guardar
                                 </button>
                             </div>
@@ -211,12 +330,21 @@ const PriceModificationModal: React.FC<PriceModificationModalProps> = ({ isOpen,
                 <div className="text-center p-2">
                     <button
                         onClick={() => setAddingPrice(true)}
-                        className="border-2 rounded-full text-4xl w-12 text-white bg-gray-600 border-gray-950 pb-1">
+                        className="border-2 rounded-full text-4xl w-12 text-white bg-gray-600 border-gray-950 pb-1"
+                    >
                         +
                     </button>
                 </div>
                 {showMessage && (
-                    <p className={`${statusMessage.includes("éxito") ? 'text-green-500' : 'text-red-500'} text-lg`}>{statusMessage}</p>
+                    <p
+                        className={`${
+                            statusMessage.includes("éxito")
+                                ? "text-green-500"
+                                : "text-red-500"
+                        } text-lg`}
+                    >
+                        {statusMessage}
+                    </p>
                 )}
             </div>
         </div>
--- frontend/src/app/components/Modals/report-error-modal.tsx
+++ frontend/src/app/components/Modals/report-error-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axiosInstance from "@/utils/axios_instance";
 
 interface ReportErrorModalProps {
@@ -6,33 +6,44 @@ interface ReportErrorModalProps {
     onClose: () => void;
 }
 
-const ReportErrorModal: React.FC<ReportErrorModalProps> = ({ isOpen, onClose }) => {
-
-    const [errorMessage, setErrorMessage] = useState<string>('');
-    const [message, setMessage] = useState<string>('');
-    const [messageType, setMessageType] = useState<'success' | 'error' | ''>('');
+const ReportErrorModal: React.FC<ReportErrorModalProps> = ({
+    isOpen,
+    onClose,
+}) => {
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [messageType, setMessageType] = useState<"success" | "error" | "">(
+        ""
+    );
+    const [loading, setLoading] = useState(false);
 
     async function sendErrorReport(message: string) {
-        axiosInstance.post('/user/report', { message: message })
+        setLoading(true);
+
+        axiosInstance
+            .post("/user/report", { message: message })
             .then(() => {
-                setMessage('Informe de error enviado exitosamente.');
-                setMessageType('success');
-                setErrorMessage('');
+                setMessage("Informe de error enviado exitosamente.");
+                setMessageType("success");
+                setErrorMessage("");
             })
             .catch(() => {
-                setMessage('Ha ocurrido un error al intentar enviar el reporte, inténtelo de nuevo más tarde.');
-                setMessageType('error');
-                setErrorMessage('');
-            });
+                setMessage(
+                    "Ha ocurrido un error al intentar enviar el reporte, inténtelo de nuevo más tarde."
+                );
+                setMessageType("error");
+                setErrorMessage("");
+            })
+            .finally(() => setLoading(false));
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
         if (errorMessage.trim().length === 0) {
-            setMessage('Error: El mensaje de error no puede estar vacío.');
-            setMessageType('error');
-            return
+            setMessage("Error: El mensaje de error no puede estar vacío.");
+            setMessageType("error");
+            return;
         }
 
         sendErrorReport(errorMessage);
@@ -42,21 +53,36 @@ const ReportErrorModal: React.FC<ReportErrorModalProps> = ({ isOpen, onClose }) 
         isOpen && (
             <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
                 <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-                    <h2 className="text-lg font-bold mb-4">Reportar un Error</h2>
+                    <h2 className="text-lg font-bold mb-4">
+                        Reportar un Error
+                    </h2>
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
-                            <label className="block text-sm font-medium mb-1" htmlFor="error-message">Descripción del Error</label>
+                            <label
+                                className="block text-sm font-medium mb-1"
+                                htmlFor="error-message"
+                            >
+                                Descripción del Error
+                            </label>
                             <textarea
                                 id="error-message"
                                 value={errorMessage}
-                                onChange={(e) => setErrorMessage(e.target.value)}
+                                onChange={(e) =>
+                                    setErrorMessage(e.target.value)
+                                }
                                 className="border border-gray-300 p-2 rounded w-full"
                                 rows={4}
                                 required
                             />
                         </div>
                         {message && (
-                            <div className={`mb-4 p-2 rounded ${messageType === 'success' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>
+                            <div
+                                className={`mb-4 p-2 rounded ${
+                                    messageType === "success"
+                                        ? "bg-green-100 text-green-700"
+                                        : "bg-red-100 text-red-700"
+                                }`}
+                            >
                                 {message}
                             </div>
                         )}
@@ -70,9 +96,29 @@ const ReportErrorModal: React.FC<ReportErrorModalProps> = ({ isOpen, onClose }) 
                             </button>
                             <button
                                 type="submit"
-                                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                                className="flex px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                             >
-                                Enviar
+                                {loading && (
+                                    <svg
+                                        className="animate-spin h-5 w-5 mr-3"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        ></path>
+                                    </svg>
+                                )}
+                                {!loading ? "Enviar" : "Procesando..."}
                             </button>
                         </div>
                     </form>
